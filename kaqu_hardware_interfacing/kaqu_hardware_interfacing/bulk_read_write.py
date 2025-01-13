5244d8345bb0a095ee898c28a1af179101295198
Dongryun <storm5030@naver.com>
2025-01-13 17:52:34 +0900

    Delete Baudrate

--- kaqu_hardware_interfacing/kaqu_hardware_interfacing/bulk_read_write.py
+++ kaqu_hardware_interfacing/kaqu_hardware_interfacing/bulk_read_write.py
@@ -2,6 +2,35 @@
 # -*- coding: utf-8 -*-
 
 # bulk_read_write
+
+# 이 파일을 Practice 브랜치에 작업하시거나, 
+# Practice 브랜치에서 분기하셔서 본인 이름으로 branch 만들고 Practice를 pull 하시면서 작업해주시면 감사하겠습니다. 
+# Team2 브랜치에는 확정된 내용들만 merge하도록 하겠습니다. 
+
+# 개념을 요약하자면 다음과 같습니다. 
+# 기존의 Team2 브랜치는 팀의 최종 결정 후에 push하기
+# 이 브랜치(Practice)에 작업하기
+# 혹은 이 브랜치(Practice)에서 갈라지는 하나의 브랜치를 하나 더 만들어(ex : Youngjin) 
+# 거기서 작업 후 push -> 이후 merge할 때는 팀 내의 상의/혹은 팀장의 확인을 받으시면 됩니다. 
+
+# 바로 merge해도 잘 할 수 있다 싶으신 분들은 Practice에서, 
+# 약간 쫄린다 싶으시면 본인 branch 만들어서 작업하시고 Practice로 merge하기 전 팀장에게 말씀해주시면 됩니다. 
+
+# 실제로 어떻게 하느냐? 두 가지 경우로 나눠보면, 1. Practice에서 작업하는 경우, 2. 본인 브랜치를 만들어 작업하시는 경우
+# 1. Practice에서 작업하는 경우 
+# 0) 원격의 Practice를 pull
+# 1) 로컬의 Practice 브랜치에서 시작 
+# 2) 원격 저장소의 Practice 브랜치를 pull(merge) 
+# 3) 작업하기
+# 4) 커밋 후 Practice 브랜치에 푸쉬(git push origin Practice)
+
+# 2. 본인 브랜치를 만들어 작업하시는 경우  
+# 0) Practice 브랜치를 pull, 여기서 갈라지는 본인 이름 브랜치 생성
+# 1) 로컬의 [본인 이름] 브랜치에서 시작 
+# 2) 원격 저장소의 Practice 브랜치를 pull(merge) : 본인 이름 브랜치는 아무도 업데이트하지 않으므로
+# 3) 작업하기 
+# 4) [본인 이름] 브랜치에 push
+
 # 업무 분담
 # 1. 하드웨어 통신 관련 : 강동륜, 박정환
 # 1월 2주
@@ -75,18 +104,18 @@ if MY_DXL == 'X_SERIES' or MY_DXL == 'MX_SERIES':
 PROTOCOL_VERSION            = 2.0
 
 # Default setting
-FR1_ID                     = 1                             # Dynamixel ID: 1
-FR2_ID                     = 2                             # Dynamixel ID: 2
-FR3_ID                     = 3
-FL1_ID                     = 4
-FL2_ID                     = 5
-FL3_ID                     = 6
-RR1_ID                     = 7
-RR2_ID                     = 8
-RR3_ID                     = 9
-RL1_ID                     = 10
-RL2_ID                     = 11
-FL3_ID                     = 12
+FR1_ID                     = 11                             # Dynamixel ID: 1
+FR2_ID                     = 12                             # Dynamixel ID: 2
+FR3_ID                     = 13
+FL1_ID                     = 21
+FL2_ID                     = 22
+FL3_ID                     = 23
+RR1_ID                     = 31
+RR2_ID                     = 32
+RR3_ID                     = 33
+RL1_ID                     = 41
+RL2_ID                     = 42
+RL3_ID                     = 43
 
 DEVICENAME                  = "/dev/ttyUSB0".encode('utf-8')        # Check which port is being used on your controller
                                                             # ex) Windows: "COM1"   Linux: "/dev/ttyUSB0"
@@ -112,8 +141,9 @@ TORQUE_DISABLE              = 0                 # Value for disabling the torque
 DXL_MOVING_STATUS_THRESHOLD = 20                # Dynamixel moving status threshold
 
 index = 0
-dxl_goal_position = [DXL_MINIMUM_POSITION_VALUE, DXL_MAXIMUM_POSITION_VALUE]        # Goal position
+dxl_goal_position = [0]*12        # 다이나믹셀 각도로 변환된 Goal position 넣을 곳
 dxl_led_value = [0x00, 0x01]                                                        # Dynamixel LED value for write
+dxl_id = [FR1_ID, FR2_ID, FR3_ID, FL1_ID, FL2_ID, FL3_ID, RR1_ID, RR2_ID, RR3_ID, RL1_ID, RL2_ID, RL3_ID]
 
 # Initialize PortHandler instance
 # Set the port path
@@ -130,7 +160,6 @@ groupBulkWrite = GroupBulkWrite(portHandler, packetHandler)
 
 # Initialize GroupBulkRead instace for Present Position
 groupBulkRead = GroupBulkRead(portHandler, packetHandler)
-
 
 # Open port
 if portHandler.openPort():
@@ -150,37 +179,37 @@ else:
     getch()
     quit()
 
+# 각 모터 토크 켜기
+for i in dxl_id:
+    dxl_comm_result, dxl_error= packetHandler.write1ByteTxRx(portHandler, i, ADDR_TORQUE_ENABLE, TORQUE_ENABLE)
+    if dxl_comm_result != COMM_SUCCESS:
+        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+    elif dxl_error != 0:
+        print("%s" % packetHandler.getRxPacketError(dxl_error))
+    else:
+        print("Dynamixel#%d has been successfully connected" % i)
 
-# Enable Dynamixel#1 Torque
-# dynamixel.write1ByteTxRx(port_num, PROTOCOL_VERSION, DXL1_ID, ADDR_PRO_TORQUE_ENABLE, TORQUE_ENABLE)
-# if dynamixel.getLastTxRxResult(port_num, PROTOCOL_VERSION) != COMM_SUCCESS:
-#     dynamixel.printTxRxResult(PROTOCOL_VERSION, dynamixel.getLastTxRxResult(port_num, PROTOCOL_VERSION))
-# elif dynamixel.getLastRxPacketError(port_num, PROTOCOL_VERSION) != 0:
-#     dynamixel.printRxPacketError(PROTOCOL_VERSION, dynamixel.getLastRxPacketError(port_num, PROTOCOL_VERSION))
-# else:
-#     print("Dynamixel#1 has been successfully connected")
-dxl_comm_result, dxl_error= ph.write1ByteTxRx(portHandler, FR1_ID, ADDR_PRO_TORQUE_ENABLE, TORQUE_ENABLE)
-if dxl_comm_result != COMM_SUCCESS:
-    print("%s" % ph.getTxRxResult(dxl_comm_result))
+# present position에 대한 parameter 저장소 추가
+for i in dxl_id:
+    dxl_addparam_result = groupBulkRead.addParam(i, ADDR_PRESENT_POSITION, LEN_PRESENT_POSITION)
+    if dxl_addparam_result != True:
+        print("[ID:%03d] groupBulkRead addparam failed" % i)
+        quit()
 
-# Enable Dynamixel#2 Torque
-dynamixel.write1ByteTxRx(port_num, PROTOCOL_VERSION, DXL2_ID, ADDR_PRO_TORQUE_ENABLE, TORQUE_ENABLE)
-if dynamixel.getLastTxRxResult(port_num, PROTOCOL_VERSION) != COMM_SUCCESS:
-    dynamixel.printTxRxResult(PROTOCOL_VERSION, dynamixel.getLastTxRxResult(port_num, PROTOCOL_VERSION))
-elif dynamixel.getLastRxPacketError(port_num, PROTOCOL_VERSION) != 0:
-    dynamixel.printRxPacketError(PROTOCOL_VERSION, dynamixel.getLastRxPacketError(port_num, PROTOCOL_VERSION))
-else:
-    print("Dynamixel#2 has been successfully connected")
-
-# Add parameter storage for Dynamixel#1 present position value
-dxl_addparam_result = ctypes.c_ubyte(dynamixel.groupBulkReadAddParam(groupread_num, DXL1_ID, ADDR_PRO_PRESENT_POSITION, LEN_PRO_PRESENT_POSITION)).value
-if dxl_addparam_result != 1:
-    print("[ID:%03d] groupBulkRead addparam failed" % (DXL1_ID))
-    quit()
-    
 # 여기까지 초기 세팅
 # 아래의 while문 내부 내용을 msg 콜백으로 묶어서 써도 될듯
 # 하드웨어, 변환 담당 조는 ROS조가 노드를 완성하기 전까지 callback 함수 형태로 만들어 두기
+
+while 1: # 값을 보내고 받는 함수들, 이걸 callback으로 하면 될듯. 
+    print("Press any key to continue! (or press ESC to quit!)")
+    if getch() == chr(0x1b):
+        break
+
+    for i in range len(dxl_goal_position):
+        param_goal_position = [DXL_LOBYTE(DXL_LOWORD(dxl_goal_position[i]))]
+
+
+
 
 while 1:
     print("Press any key to continue! (or press ESC to quit!)")
@@ -191,6 +220,12 @@ while 1:
     dxl_addparam_result = ctypes.c_ubyte(dynamixel.groupBulkWriteAddParam(groupwrite_num, DXL1_ID, ADDR_PRO_GOAL_POSITION, LEN_PRO_GOAL_POSITION, dxl_goal_position[index], LEN_PRO_GOAL_POSITION)).value
     if dxl_addparam_result != 1:
         fprintf(stderr, "[ID:%03d] groupBulkWrite addparam failed", DXL1_ID)
+        quit()
+
+    # Add parameter storage for Dynamixel#2 LED value
+    dxl_addparam_result = ctypes.c_ubyte(dynamixel.groupBulkWriteAddParam(groupwrite_num, DXL2_ID, ADDR_PRO_LED_RED, LEN_PRO_LED_RED, dxl_led_value[index], LEN_PRO_LED_RED)).value
+    if dxl_addparam_result != 1:
+        fprintf(stderr, "[ID:%03d] groupBulkWrite addparam failed", DXL2_ID)
         quit()
 
     # Bulkwrite goal position and LED value
@@ -237,6 +272,16 @@ while 1:
         index = 0
 
 
+# ROS2 맡으신 분들은 여기에 노드 작성해주시면 됩니다. 
+
+
+# 노드 콜백함수 정의
+# ROS2 맡으신 분들은 일단 비워두시고, 하드웨어 통신+변환 맡으신 분들은 일반적인 함수 형태로 작성해주시면 됩니다. 
+
+# 
+
+
+
 # Disable Dynamixel#1 Torque
 dynamixel.write1ByteTxRx(port_num, PROTOCOL_VERSION, DXL1_ID, ADDR_PRO_TORQUE_ENABLE, TORQUE_DISABLE)
 if dynamixel.getLastTxRxResult(port_num, PROTOCOL_VERSION) != COMM_SUCCESS:
@@ -253,3 +298,6 @@ elif dynamixel.getLastRxPacketError(port_num, PROTOCOL_VERSION) != 0:
 
 # Close port
 dynamixel.closePort(port_num)
+
+
+# 여기다가 노드 작성해주세요
